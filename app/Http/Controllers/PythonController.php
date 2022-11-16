f6ab0f8c2c5b23ef05f5a08be0a0ae04912d4297
CarlosMontesdeoca <mateo.game@hotmail.com>
2022-11-16 16:33:10 -0500

    python

--- app/Http/Controllers/PythonController.php
+++ app/Http/Controllers/PythonController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class PythonController extends Controller
 {
     public function run($codPro){
-        $command = escapeshellcmd('python C:\Tester\Python\testpython.py' . " {$codPro}");
+        $command = escapeshellcmd('python C:\xampp\htdocs\SQLServer\SendProject.py' . " {$codPro}");
         $output = shell_exec($command);
         return $output;
     }
