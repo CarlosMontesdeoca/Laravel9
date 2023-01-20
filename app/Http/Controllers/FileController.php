f7a74159e51c92f603618dd201549a271044130c
CarlosMontesdeoca <mateo.game@hotmail.com>
2023-01-20 14:45:15 -0500

    upload files

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -2,18 +2,34 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facade\File;
+use Illuminate\Support\Facade\Validator;
 use Illuminate\Http\Request;
+use App\Models\File;
+use Illuminate\Support\Facades\Storage;
 
 class FileController extends Controller
 {
     function upload(Request $request){
-        $result = $request->file('file')->store('Archivos');
-        return [ "result"->$result];
+
+        $request->validate([
+            'name' => 'required|string|unique:files|max:100',
+            'file' => 'required|mimes:pdf',
+        ]);
+
+//        $article = Article::create($request->all());
+
+        $file = new File($request->all());
+        $path = $request->file->store('public/reports');
+//        $path = $request->image->storeAs('public/articles', $request->user()->id . '_' . $article->title . '.' . $request->image->extension());
+
+        $file->path = 'reports/' . basename($path);
+        $file->save();
+
+        return response()->json($file, 201);
     }
 
     function download(Request $request){
-        $path = storage_path('app\Archivos\ ');
-        return respose()->download($path);
+        // $path = storage_path('app\Archivos\ ');
+        // return respose()->download($path);
     }
 }
